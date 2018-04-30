9df5e41fb601ed762a4e1450c5b321b79f8ce0c7
Masayuki Ono <mono0926@gmail.com>
2018-04-30 12:17:08 +0900

    Add test files

---
+++ functions/lib/test/index.test.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai = require("chai");
+const assert = chai.assert;
+describe('DESC', () => {
+    it('IT', () => {
+        // assert.equal("foo", "bar");
+        assert.equal("foo", "foo");
+    });
+});
+//# sourceMappingURL=index.test.js.map
---
+++ functions/src/test/index.test.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+const assert = chai.assert;
+
+describe('DESC', () => {
+  it('IT', () => {
+    // assert.equal("foo", "bar");
+    assert.equal("foo", "foo");
+  });
+});
